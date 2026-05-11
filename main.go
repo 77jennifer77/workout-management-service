e94ef6d1b06bcc467737b28b78603e7816595561
Jennifer Chiang <77jennifer77@gmail.com>
2026-05-11 09:59:04 -0700

    Add get route for workouts

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ import (
 func main() {
 	e := echo.New()
     e.POST("/create", create)
+    e.POST("/get", get)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func create(c echo.Context) error {
 	return c.String(http.StatusOK, "Workout created")
+}
+
+func get(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Getting workout")
 }
