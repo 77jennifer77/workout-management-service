519d380c768c73ef505e9dd30b6859caeb0d7682
Jennifer Chiang <77jennifer77@gmail.com>
2026-05-11 10:06:46 -0700

    Change internal workout.go contents

--- internal/workout/workout.go
+++ internal/workout/workout.go
@@ -1,12 +1,6 @@
 package workout
 
-import (
-	"github.com/SamirMarin/workout-management-service/internal/dynamodb"
-	"github.com/aws/aws-sdk-go/aws"
-	awsDynamoDb "github.com/aws/aws-sdk-go/service/dynamodb"
-	awsDynamoDbAttribute "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"strconv"
-)
+import "fmt"
 
 type Workout struct {
 	Owner     string     `json:"owner"`
@@ -24,87 +18,18 @@ type Equipment struct {
 type Exercise struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
-	Sets        int    `json:"sets"`
+	Sets        int    `json:"reps"`
 	Time        int    `json:"time"`
 }
 
-var tableName = "Workout"
-
 // CreateWorkout creates a workout, save new workout to db
 func (w *Workout) CreateWorkout() error {
-	dynamoDbClient := dynamodb.NewClient(tableName)
-	err := dynamoDbClient.StoreItem(w)
-	if err != nil {
-		return err
-	}
+	fmt.Println(w)
 	return nil
 }
+
+// GetWorkout gets a workout from db
 func (w *Workout) GetWorkout() error {
-	dynamoDbClient := dynamodb.NewClient(tableName)
-	err, getItemOutput := dynamoDbClient.GetItem(w)
-	if err != nil {
-		return err
-	}
-	err = awsDynamoDbAttribute.UnmarshalMap(getItemOutput.Item, w)
+	fmt.Println(w)
 	return nil
-}
-
-func (w *Workout) ToDynamoDbAttribute() map[string]*awsDynamoDb.AttributeValue {
-	exerciseList := make([]*awsDynamoDb.AttributeValue, len(w.Exercises))
-	for i, exercise := range w.Exercises {
-		exerciseList[i] = &awsDynamoDb.AttributeValue{
-			M: map[string]*awsDynamoDb.AttributeValue{
-				"Name": {
-					S: aws.String(exercise.Name),
-				},
-				"Description": {
-					S: aws.String(exercise.Description),
-				},
-				"Sets": {
-					N: aws.String(strconv.Itoa(exercise.Sets)),
-				},
-				"Time": {
-					N: aws.String(strconv.Itoa(exercise.Time)),
-				},
-			},
-		}
-	}
-	return map[string]*awsDynamoDb.AttributeValue{
-		"Owner": {
-			S: aws.String(w.Owner),
-		},
-		"Name": {
-			S: aws.String(w.Name),
-		},
-		"Category": {
-			S: aws.String(w.Category),
-		},
-		"Equipment": {
-			M: map[string]*awsDynamoDb.AttributeValue{
-				"Name": {
-					S: aws.String(w.Equipment.Name),
-				},
-				"Description": {
-					S: aws.String(w.Equipment.Description),
-				},
-			},
-		},
-		"Exercises": {
-			L: exerciseList,
-		},
-	}
-}
-
-func (w *Workout) ToDynamoDbItemInput() *awsDynamoDb.GetItemInput {
-	return &awsDynamoDb.GetItemInput{
-		TableName: aws.String(tableName),
-		Key: map[string]*awsDynamoDb.AttributeValue{
-			"Owner": {
-				S: aws.String(w.Owner),
-			},
-			"Name": {
-				S: aws.String(w.Name),
-			},
-		},
-	}
 }
